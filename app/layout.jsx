b244b8608c85bf7a79b08be85c9cb37b60329e1c
Princewill Ubido <princewillubidoo@gmail.com>
2026-01-22 04:24:09 +0100

    favicon update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,11 @@
 import "./globals.css";
-
 export const metadata = {
   title: "SkillTrack",
   description: "Your Ultimate Skill Management Solution",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -13,10 +14,9 @@ export default function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" type="image/png" href="/favicon.png" />
       </head>
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
